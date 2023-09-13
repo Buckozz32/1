c6dcf36e65588325f1e5bba3bfd2d8ce96a2a0a4
Buckozz32 <144051962+Buckozz32@users.noreply.github.com>
2023-09-13 10:24:10 +0300

    Create Main.go

---
+++ Main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	helloworldpb "github.com/myuser/myrepo/proto/helloworld"
+)
+
+type server struct{
+	helloworldpb.UnimplementedGreeterServer
+}
+
+func NewServer() *server {
+	return &server{}
+}
+
+func (s *server) SayHello(ctx context.Context, in *helloworldpb.HelloRequest) (*helloworldpb.HelloReply, error) {
+	return &helloworldpb.HelloReply{Message: in.Name + " world"}, nil
+}
+
+func main() {
+	
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("Failed to listen:", err)
+	}
+
+	
+	s := grpc.NewServer()
+helloworldpb.RegisterGreeterServer(s, &server{})
+	log.Println("Serving gRPC on 0.0.0.0:8080")
+	log.Fatal(s.Serve(lis))
